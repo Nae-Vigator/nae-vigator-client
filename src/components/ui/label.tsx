12fafcdcdcb6cee2b82861d2f5e57a0a3b8a3e71
presentKey <kim.hyeonjae4@gmail.com>
2025-10-30 02:33:21 +0900

    design: Label 공통 스타일 변경

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -16,6 +16,7 @@ function Label({
       className={cn(
         'flex items-center text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
         isRequired && "after:content-['*'] after:text-destructive after:ml-0.5",
+        'mb-1',
         className,
       )}
       {...props}
