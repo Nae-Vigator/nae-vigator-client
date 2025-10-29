a163185dd05817823bbba5817d8391e00879f26d
presentKey <kim.hyeonjae4@gmail.com>
2025-10-30 01:26:44 +0900

    feat: shadcn의 alert-dialog, command, popover, textarea, label 컴포넌트 설치

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import * as LabelPrimitive from '@radix-ui/react-label';
+
+import { cn } from '@/lib/utils';
+
+function Label({
+  isRequired = false,
+  className,
+  ...props
+}: React.ComponentProps<typeof LabelPrimitive.Root> & {
+  isRequired?: boolean;
+}) {
+  return (
+    <LabelPrimitive.Root
+      data-slot="label"
+      className={cn(
+        'flex items-center text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+        isRequired && "after:content-['*'] after:text-destructive after:ml-0.5",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Label };
