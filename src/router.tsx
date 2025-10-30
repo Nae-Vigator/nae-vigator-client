2c8e8e1b3adb623a9c2a5d46cb50dd352310b3b3
presentKey <kim.hyeonjae4@gmail.com>
2025-10-30 23:34:15 +0900

    chore: router 설정 변경

--- src/router.tsx
+++ src/router.tsx
@@ -4,7 +4,8 @@ import NotFound from './pages/NotFound/NotFound';
 import Companies from './pages/Companies/Companies';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import App from './App';
+import Home from './pages/Home/Home';
+
 /**
  * @link https://reactrouter.com/start/data/routing
  */
@@ -14,11 +15,10 @@ const router = createBrowserRouter([
     element: <AppRoot />,
     errorElement: <NotFound />,
     children: [
-      { index: true, element: <Login /> },
+      { index: true, element: <Home /> },
+      { path: 'login', element: <Login /> },
       { path: 'signup', element: <Signup /> },
-      { path: 'app', element: <App /> },
       { path: 'companies', element: <Companies /> },
-      ,
     ],
   },
 ]);
