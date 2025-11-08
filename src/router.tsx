5de1c394fb059d0f21f686adc18c205c4aa74271
jun-hwan00 <0528klkl@naver.com>
2025-11-08 17:42:15 +0900

    Revert "Merge pull request #23 from Nae-Vigator/feature/17-user-info-page"
    This reverts commit ebfbcac75ce01479ff6cca6e724d081aec8ac9c1, reversing
    changes made to 783b24cb00db2612ae618e27e190741586a1367e.

--- src/router.tsx
+++ src/router.tsx
@@ -6,7 +6,10 @@ import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
 import HomePage from './pages/HomePage/HomePage';
 import MainRoot from './components/layout/MainRoot/MainRoot';
-import UserInfoPage from './pages/User-infoPage/User-infoPage';
+import TestPage from './pages/SignupPage/test';
+import CompaniesLayout from './components/layout/CompaniesLayout/CompaniesLayout';
+import ExperiencePage from './pages/CompaniesPage/ExperiencePage/ExperiencePage';
+import CoverLetterPage from './pages/CompaniesPage/CoverLetterPage/CoverLetterPage';
 /**
  * @link https://reactrouter.com/start/data/routing
  */
@@ -20,12 +23,31 @@ const router = createBrowserRouter([
         element: <MainRoot />,
         children: [
           { index: true, element: <HomePage /> },
-          { path: 'companies', element: <CompaniesPage /> },
-          { path: 'user-info', element: <UserInfoPage /> },
+          {
+            path: 'companies',
+            children: [
+              { index: true, element: <CompaniesPage /> },
+              {
+                path: ':company',
+                element: <CompaniesLayout />,
+                children: [
+                  {
+                    path: 'experience',
+                    element: <ExperiencePage />,
+                  },
+                  {
+                    path: 'coverletter',
+                    element: <CoverLetterPage />,
+                  },
+                ],
+              },
+            ],
+          },
         ],
       },
       { path: 'login', element: <LoginPage /> },
       { path: 'signup', element: <SignupPage /> },
+      { path: 'test', element: <TestPage /> },
     ],
   },
 ]);
