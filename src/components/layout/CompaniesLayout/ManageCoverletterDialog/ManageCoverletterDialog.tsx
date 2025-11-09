6e052381f13b8071e7767c196ea6ee78fafdab57
presentKey <kim.hyeonjae4@gmail.com>
2025-11-09 16:32:09 +0900

    feat: 질문 추가 및 삭제 기능 구현

--- src/components/layout/CompaniesLayout/ManageCoverletterDialog/ManageCoverletterDialog.tsx
+++ src/components/layout/CompaniesLayout/ManageCoverletterDialog/ManageCoverletterDialog.tsx
@@ -12,8 +12,34 @@ import {
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Plus, X } from 'lucide-react';
+import { useState } from 'react';
 
 function ManageCoverletterDialog() {
+  const [questions, setQuestions] = useState([
+    { id: crypto.randomUUID(), title: '' },
+  ]);
+
+  const handleAddQuestionClick = () => {
+    setQuestions((prev) => [...prev, { id: crypto.randomUUID(), title: '' }]);
+  };
+
+  const handleDeleteQuestionClick = (id: string) => {
+    if (questions.length === 1) return;
+
+    setQuestions((prev) => prev.filter((q) => q.id !== id));
+  };
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    id: string,
+  ) => {
+    setQuestions((prev) =>
+      prev.map((q) => (q.id === id ? { ...q, title: e.target.value } : q)),
+    );
+  };
+
+  console.log(questions);
+
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
@@ -39,24 +65,35 @@ function ManageCoverletterDialog() {
         </AlertDialogHeader>
 
         <div className="flex flex-col gap-4 mb-10">
-          <ol>
-            <li className="flex gap-4 items-center">
-              <span className="border border-ring rounded-md size-8 flex justify-center items-center shrink-0">
-                1
-              </span>
-              <Input
-                className="h-10"
-                // value="지원하는 회사와 직무를 선택한 이유와 입사 후 어떤 목표를 가지고 있는지 말씀해 주세요."
-                placeholder="자유형식 문항이나 회사별 질문을 직접 입력해 보세요."
-              />
+          <ol className="flex flex-col gap-4">
+            {questions.map((question, i) => (
+              <li key={question.id} className="flex gap-4 items-center">
+                <span className="border border-ring rounded-md size-8 flex justify-center items-center shrink-0">
+                  {i + 1}
+                </span>
+                <Input
+                  className="h-10"
+                  value={question.title}
+                  placeholder="자유형식 문항이나 회사별 질문을 직접 입력해 보세요."
+                  onChange={(e) => handleInputChange(e, question.id)}
+                />
 
-              <Button variant={'secondary'} size={'icon-lg'}>
-                <X />
-              </Button>
-            </li>
+                <Button
+                  variant={'secondary'}
+                  size={'icon-lg'}
+                  onClick={() => handleDeleteQuestionClick(question.id)}
+                >
+                  <X />
+                </Button>
+              </li>
+            ))}
           </ol>
 
-          <Button size={'icon-lg'} className="self-center">
+          <Button
+            size={'icon-lg'}
+            className="self-center"
+            onClick={handleAddQuestionClick}
+          >
             <Plus />
           </Button>
         </div>
