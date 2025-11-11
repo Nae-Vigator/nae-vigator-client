6537b45e08dcd9fb7c0ad50b0858024ba9f9937c
presentKey <kim.hyeonjae4@gmail.com>
2025-11-11 23:19:42 +0900

    feat: tanstack query의 default option 설정

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,14 @@ import router from './router.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 60 * 1000 * 5,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
